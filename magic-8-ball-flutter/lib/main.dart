2ca4ab4b629bda233fed6e8b2f31de33ac87c18d
Nirbhay <nirbhaymakhija@gmail.com>
2021-07-21 01:08:25 +0530

    app completed

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade500,
+          title: Text('Ask Me Anything'),
+          centerTitle: true,
+        ),
+        body: EightBall(),
+      ),
+    ),
+  );
+}
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int answerNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            answerNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$answerNumber.png'),
       ),
     );
+  }
+}
