30c5379889f38f78231a7281142c2b8ea7c1cd3e
Nirbhay <nmakhija_be20@thapar.edu>
2021-08-08 03:36:19 +0530

    added dropdownmenu for android and picker for iOS

--- bitcoin_ticker_flutter/lib/prime_screen.dart
+++ bitcoin_ticker_flutter/lib/prime_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +9,69 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  // for android
+  DropdownButton<String> getDropDownItems() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropDownItems.add(newItem);
+    }
+    return DropdownButton<String>(
+      autofocus: true,
+      focusColor: Color(0xFF757575),
+      elevation: 5,
+      value: selectedCurrency,
+      items: dropDownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    );
+  }
+
+  // for iOS
+  CupertinoPicker getiOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      var newItem = Text(currency);
+      pickerItems.add(newItem);
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedItem) {
+        print(selectedItem);
+      },
+      children: pickerItems,
+    );
+  }
+
+  Widget getList() {
+    if (Platform.isAndroid) {
+      return getDropDownItems();
+    } else if (Platform.isIOS) {
+      return getiOSPicker();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF424242),
       appBar: AppBar(
-        title: Text('💸 Coin Ticker'),
+        title: Text(
+          '💲 Coin Ticker',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFF212121),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -19,7 +80,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Color(0xFF616161),
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -41,8 +102,8 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            color: Color(0xFF616161),
+            child: getList(),
           ),
         ],
       ),
