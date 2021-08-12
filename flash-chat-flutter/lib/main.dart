b6102c70e0a62f7f45481376eef1438f800c7088
Nirbhay <nmakhija_be20@thapar.edu>
2021-08-12 13:39:39 +0530

    routes added

--- flash-chat-flutter/lib/main.dart
+++ flash-chat-flutter/lib/main.dart
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        ChatScreen.id: (ctx) => ChatScreen(),
+        LoginScreen.id: (ctx) => LoginScreen(),
+        RegistrationScreen.id: (ctx) => RegistrationScreen(),
+        WelcomeScreen.id: (ctx) => WelcomeScreen(),
+      },
     );
   }
 }
