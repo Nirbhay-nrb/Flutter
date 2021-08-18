01c6d34a7eb90f14615c2d4a7bf5633397ed8335
Nirbhay <nmakhija_be20@thapar.edu>
2021-08-19 01:12:33 +0530

    app completed

--- todoey_flutter/lib/models/task_data.dart
+++ todoey_flutter/lib/models/task_data.dart
@@ -4,11 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> _tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
-  ];
+  List<Task> _tasks = [];
 
   int get taskCount {
     return _tasks.length;
